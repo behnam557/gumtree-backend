512de9c5db432910b1d918d31b3b9ae39aa64777
behnam557 <behnam557@googlemail.com>
2026-05-06 19:10:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,22 @@ require("./db");
 const app = express();
 
 app.use(cors());
+
+app.use(
+  "/api/billing/webhook",
+  express.raw({ type: "application/json" })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/billing", require("./routes/billing"));
 app.use("/api/licence", require("./routes/licence"));
+
 app.get("/", (req, res) => {
   res.send("Backend is working");
 });
+
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running on port " + (process.env.PORT || 5000));
 });
