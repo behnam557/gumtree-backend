d666541f5dc866e3278b75c0c2752c4d046d1d5a
behnam557 <behnam557@googlemail.com>
2026-05-18 20:52:48 +0100

    Update billing.js

--- routes/billing.js
+++ routes/billing.js
@@ -10,9 +10,13 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT || 587),
   secure: String(process.env.SMTP_SECURE || "false") === "true",
+  family: 4,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    servername: process.env.SMTP_HOST,
   },
 });
 
